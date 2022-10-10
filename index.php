6eab2bc1eb4de80c5d6fb0652b3888cfab63995b
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-10-10 14:32:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Patient Portal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h2>Hello! Welcome to the Patient Portal</h2>
+    <h6>On this website you can find information about your patient, their dotor, and what day their appointment is on!</h6>
+  </body>
+    <a class="btn btn-primary" href="Patient.php" role="button">Patient Information</a>
+    <a class="btn btn-primary" href="Doctor.php" role="button">Doctor Information</a>
+    <a class="btn btn-primary" href="Appointment.php" role="button">Appointment Information</a>
+    <a class="btn btn-primary" href="Staff.php" role="button">Staff Information</a>
+  <a class="btn btn-primary" href="Patient-Cards.php" role="button">Show Patient Detials</a>
+    
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
