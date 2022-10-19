6ac0ef84ffece0793ee0de2ee6c155ff3bd293cf
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-10-19 12:26:20 -0500

    Create patient-edit-save.php

---
+++ patient-edit-save.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Patient</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php
+$servername = "localhost";
+$username = "rebeccca_ruser";
+$password = "WL]8Dmr[Qag6";
+$dbname = "rebeccca_MIS";
+    
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$pName = $_POST['pName'];
+
+$sql = "update Patient set FirstName=? where PatientID=?";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("si", $pName, $_POST['pid']);
+    $stmt->execute();
+?>
+    
+    <h1>Edit Patient</h1>
+<div class="alert alert-success" role="alert">
+  Patient edited.
+</div>
+    <a href="Patient.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
