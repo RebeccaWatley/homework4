df67e72727a86d9427611ed37a6e4b7414fee7c0
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-10-24 12:39:50 -0500

    Create doctor-add.php

---
+++ doctor-add.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Doctor</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Add Doctor</h1>
+<form method="post" action="doctor-add-save.php">
+  <div class="mb-3">
+    <label for="DoctorName" class="form-label">Doctor Name</label>
+    <input type="text" class="form-control" id="DoctorID" aria-describedby="nameHelp" name="dName">
+    <div id="nameHelp" class="form-text">Enter the doctor's name.</div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
