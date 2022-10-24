fe8e8213938720c8b377d96b6b4ab0732430e44a
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-10-24 14:09:38 -0500

    Create appointment-edit.php

---
+++ appointment-edit.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Appointment</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Edit Appointment</h1>
+<?php
+$servername = "localhost";
+$username = "rebeccca_ruser";
+$password = "WL]8Dmr[Qag6";
+$dbname = "rebeccca_MIS";
+    
+    // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ApptID, ApptDay from Appointment where ApptID=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("p", $_POST['aid']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+<form method="post" action="appointment-edit-save.php">
+  <div class="mb-3">
+    <label for="ApptDay" class="form-label">Day</label>
+    <input type="text" class="form-control" id="ApptDay" aria-describedby="nameHelp" name="aName" value="<?=$row['ApptDay']?>">
+    <div id="nameHelp" class="form-text">Enter the Appointment's day.</div>
+  </div>
+  <input type="hidden" name="aid" value="<?=$row['ApptID']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
