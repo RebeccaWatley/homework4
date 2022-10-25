431d87258bf7455309f8b369d343aa5da2c57fd3
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-10-25 10:57:24 -0500

    Update Appointment.php

--- Appointment.php
+++ Appointment.php
@@ -108,9 +108,12 @@ if ($result->num_rows > 0) {
         $selText = "";
       }
 ?>
-  <option value="<?=$ApptRow['DoctorID']?>"<?=$selText?>><?=$ApptRow['DoctorID']?></option>                         
-                          
-                        <input type="hidden" name="did" value="<?=$row['DoctorID']?>">
+  <option value="<?=$ApptRow['DoctorID']?>"<?=$selText?>><?=$ApptRow['DoctorID']?></option>    
+             
+<?php
+    }
+?>                         
+                        <input type="hidden" name="id" value="<?=$row['DoctorID']?>">
                         <input type="hidden" name="saveType" value="Edit">
                         <input type="submit" class="btn btn-primary" value="Submit">
                       </form>
